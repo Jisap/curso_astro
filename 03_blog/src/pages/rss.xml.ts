b0a9cde7935c5a68a1ed258795794faba4a33563
Jorge Matos <jisapjisap@gmail.com>
2024-08-10 16:44:53 +0200

    Imagen y contenido en el news feed

--- 03_blog/src/pages/rss.xml.ts
+++ 03_blog/src/pages/rss.xml.ts
@@ -4,8 +4,12 @@ import rss from '@astrojs/rss';
 
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
+import markdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
 
-export const GET: APIRoute = async({ params, request, site }) => {
+const parser = new markdownIt()
+
+export const GET: APIRoute = async ({ params, request, site }) => {
 
   const blogPosts = await getCollection('blog');
 
@@ -16,14 +20,30 @@ export const GET: APIRoute = async({ params, request, site }) => {
     title: 'Jisap’s Blog',
 
     description: 'Blog de aprendizaje con Astro',
-  
+
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
+
     site: site!,
-   
-    items: blogPosts.map(({ data, slug }) => ({
+
+    items: blogPosts.map(({ data, slug, body }) => ({
       title: data.title,
       pubDate: data.date,
       description: data.description,
       link: `posts/${slug}`,
+
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+
+      customData: `<media:content
+        width="${data.image.width}"
+        height="${data.image.height}"
+        medium="image"
+        url="${site + data.image.src}" />
+        type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+      `,
     })),
 
     customData: `<language>es-es</language>`,
