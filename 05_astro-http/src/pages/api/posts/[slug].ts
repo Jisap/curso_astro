6ae842a32b945da7d6293f6eac3f9f056b38ff8f
Jorge Matos <jisapjisap@gmail.com>
2024-08-13 16:46:23 +0200

    post, put, delete endpoints

--- 05_astro-http/src/pages/api/posts/[slug].ts
+++ 05_astro-http/src/pages/api/posts/[slug].ts
@@ -3,6 +3,7 @@ import type { APIRoute } from "astro";
 import { getEntry } from "astro:content";
 
 
+
 export const prerender = false
 
 export const GET: APIRoute = async({ params, request }) => {
@@ -21,6 +22,66 @@ export const GET: APIRoute = async({ params, request }) => {
   }
 
   return new Response(JSON.stringify(post), { 
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+export const POST: APIRoute = async ({ params, request }) => {
+
+  const body = await request.json()
+
+  return new Response(JSON.stringify({
+    method: 'POST',
+    ...body,
+  }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+export const PUT: APIRoute = async ({ params, request }) => {
+
+  const body = await request.json()
+
+  return new Response(JSON.stringify({
+    method: 'PUT',
+    ...body,
+  }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+export const PATCH: APIRoute = async ({ params, request }) => {
+
+  const body = await request.json()
+
+  return new Response(JSON.stringify({
+    method: 'PATCH',
+    ...body,
+  }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+
+  const { slug } = params;
+
+  return new Response(JSON.stringify({
+    method: 'DELETE',
+    slug: slug,
+  }), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
