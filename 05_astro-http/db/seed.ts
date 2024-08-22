d14d091dc28954c24b601dc0b0867f40e014faaf
Jorge Matos <jisapjisap@gmail.com>
2024-08-22 15:46:24 +0200

    Estructura y Seed

--- 05_astro-http/db/seed.ts
+++ 05_astro-http/db/seed.ts
@@ -1,4 +1,5 @@
-import { Clients, db } from 'astro:db';
+import { getCollection } from 'astro:content';
+import { Clients, db, Posts } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -9,6 +10,16 @@ export default async function seed() {
 		{ id: 3, name: "Andres", age: 23, isActive: true },
 		{ id: 4, name: "Rogelio", age: 44, isActive: true },
 	]);
+
+	const posts = await getCollection('blog');
+
+	await db.insert(Posts).values(
+		posts.map( p => ({
+			id: p.id,
+			title: p.data.title,
+			likes: Math.round( Math.random() * 100 ),
+		}))
+	)
 
 
 	console.log("seed executed");
