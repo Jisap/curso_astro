c8caca0a7997e337dfdbb5b8b79d4e5a8319c55a
Jorge Matos <jisapjisap@gmail.com>
2024-08-22 17:15:59 +0200

    Put - Incrementear la cantidad de likes

--- 05_astro-http/src/pages/api/likes/[id].ts
+++ 05_astro-http/src/pages/api/likes/[id].ts
@@ -32,3 +32,32 @@ export const GET: APIRoute = async({ params, request}) => {
     }
   })
 }
+
+
+
+export const PUT: APIRoute = async({ params, request}) => {
+
+  const postId = params.id ?? '';
+  
+  const posts = await db.select().from(Posts).where(eq(Posts.id, postId));
+  const { likes = 0 } = await request.json();
+
+  if (posts.length === 0) {                   // Sino existe 
+    const newPost = {                         // se crea
+      id: postId,
+      title: 'Post not found',
+      likes: 0,
+    };
+
+    await db.insert(Posts).values(newPost);   // y se inserta en db Posts
+    posts.push(newPost);                      // Localmente se añade en posts []
+  }
+
+  const post = posts.at(0)!;                  // Pero si existe el post obtenemos el post que se buscaba para incrementar los likes
+  post.likes = post.likes + likes             // le incrementamos los likes
+  await db.update(Posts).set(post).where(     // y actualizamos la db
+    eq(Posts.id, postId)
+  )
+
+  return new Response('Ok!', { status: 200 })
+}
