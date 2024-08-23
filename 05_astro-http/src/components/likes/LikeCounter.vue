e47193e990a639003c37c083c3c883ae8f299cb3
Jorge Matos <jisapjisap@gmail.com>
2024-08-23 15:28:46 +0200

    Props y cantidad de likes

--- 05_astro-http/src/components/likes/LikeCounter.vue
+++ 05_astro-http/src/components/likes/LikeCounter.vue
@@ -1,16 +1,49 @@
 <template>
 
-  <button @click="likePost">
-    Like Counter
+  <div v-if="isLoading">
+    Loading..
+  </div>
+
+  <button v-else-if="likeCount === 0" @click="likePost">
+    Like this post
+  </button>
+
+  <button v-else @click="likePost">
+    Likes
+    <span>{{ likeCount }}</span>
   </button>
 
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+
+  interface Props {
+    postId: string;
+  }
+
+  const props = defineProps<Props>() // Se definen las props en vue (props.postId) que se reciben
+ 
+  const likeCount = ref(0);     // Valor en bd
+  const likeClicks = ref(0);    // Cantidad de likes al darle click
+  const isLoading = ref(true);
 
   const likePost = () => {
     console.log("+1 like");
   }
+
+  const getCurrentLikes = async() => {
+    const resp = await fetch(`/api/posts/likes/${props.postId}`);
+    if(!resp.ok) return
+
+    const data = await resp.json()
+    
+    likeCount.value = data.likes;
+    isLoading.value = false;
+  }
+
+  getCurrentLikes();
 
 </script>
 
