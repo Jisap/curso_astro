1f45674f22d66d630e8be9a27cf9d92d3b16c09e
Jorge Matos <jisapjisap@gmail.com>
2024-09-28 15:54:24 +0200

    Grabar y leer cookies

--- 07_store/src/utils/cart-cookies.ts
+++ 07_store/src/utils/cart-cookies.ts
@@ -1,16 +1,40 @@
 import type { CartItem } from "@/interfaces"
-
+import Cookies from "js-cookie";
 
 
 
 export class CartCookiesClient {
+
   static getCart():CartItem[]{
-    return []
+    return JSON.parse(Cookies.get('cart') ?? '[]')  // Productos del carrito
   }
-  static addItem(): CartItem[]{
-    return []
+  
+  static addItem(cartItem:CartItem): CartItem[]{    
+    
+    const cart = CartCookiesClient.getCart()
+    const existsItemInCart = cart.find(             // Comprobamos si el producto que se añade(cartItem) ya existe en el carrito
+      (item) => item.productId === cartItem.productId && item.size === cartItem.size
+    )
+
+    if(existsItemInCart){                           // Si existe, actualizamos la cantidad de productos
+      existsItemInCart.quantity += cartItem.quantity
+    }else{
+      cart.push(cartItem)                           // Si no existe, añadimos el producto al carrito
+    }
+
+    Cookies.set('cart', JSON.stringify(cart))       // Actualizamos el cookie con el nuevo carrito
+
+    return cart
   }
-  static removeItem(): CartItem[]{
-    return []
+  static removeItem( productId:string, size:string): CartItem[]{
+    const cart = CartCookiesClient.getCart()
+
+    const updatedCart = cart.filter(
+      (item) => !(item.productId !== productId && item.size !== size)  // Filtramos los productos que no sean el producto que se quiere eliminar
+    )
+
+    Cookies.set('cart', JSON.stringify(updatedCart))                  // Actualizamos el cookie con el nuevo carrito
+    
+    return updatedCart;
   }
 }
